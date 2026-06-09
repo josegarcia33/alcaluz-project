6968cdb6903ea175d5e89d2c50a5ef6df13eee6e
CoradoSSelvin <cm23042@ues.edu.sv>
2026-06-09 12:18:14 -0600

    archivo repetido y vacío borrado(template)

--- alcaluz/sistema/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import vistas
-
-urlpatterns = [
-    path('', vistas.inicio, name='inicio'),
-]
--- alcaluz/sistema/vistas.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def inicio(request):
-    return render(request, 'index.html')
