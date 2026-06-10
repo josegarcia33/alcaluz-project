92376c51b90679a859396354ecc621df4b82f1ca
CoradoSSelvin <cm23042@ues.edu.sv>
2026-06-09 19:51:35 -0600

    actualizacion en models.py

--- alcaluz/usuario/models.py
+++ alcaluz/usuario/models.py
@@ -7,120 +7,143 @@ from django.contrib.auth.models import User
 
 
 class Departamento(models.Model):
-    # pk
-    DepartamentoID = models.AutoField(primary_key=True, verbose_name="ID Departamento")
-    nombre = models.CharField(max_length=100, unique=True, verbose_name="Nombre del Departamento")
+    id_departamento = models.AutoField(primary_key=True)
+    nom_departamento = models.CharField(max_length=255)
 
     class Meta:
-        verbose_name = "Departamento"
-        verbose_name_plural = "Departamentos"
-        db_table = 'Departamentos'
+        db_table = 'Departamento'
+
+    def __str__(self):
+        return self.nom_departamento
+
+
+class Municipio(models.Model):
+    id_municipio = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+
+    departamento = models.ForeignKey(
+        Departamento,
+        on_delete=models.PROTECT,
+        db_column='id_departamento'
+    )
+
+    class Meta:
+        db_table = 'Municipio'
 
     def __str__(self):
         return self.nombre
 
 
-class Municipio(models.Model):
-    MunicipioID = models.AutoField(primary_key=True, verbose_name="ID Municipio")
-    nombre = models.CharField(max_length=100, verbose_name="Nombre del Municipio")
-    departamento = models.ForeignKey(
-        Departamento, 
-        on_delete=models.PROTECT, 
-        related_name="municipios", 
-        verbose_name="Departamento al que pertenece",
-        db_column='DepartamentoID' 
-    )
-
-    class Meta:
-        verbose_name = "Municipio"
-        verbose_name_plural = "Municipios"
-        db_table = 'Municipios'
-        unique_together = ('nombre', 'departamento')
-
-    def __str__(self):
-        return f"{self.nombre} ({self.departamento.nombre})"
-
-
 class Zona(models.Model):
-    ZonaID = models.AutoField(primary_key=True, verbose_name="ID Zona")
-    nombre = models.CharField(max_length=100, verbose_name="Nombre del Zona/Sector")
-    
-    # Relación 
+    id_zona = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    tipo_zona = models.CharField(max_length=255, blank=True, null=True)
+
     municipio = models.ForeignKey(
-        Municipio, 
-        on_delete=models.PROTECT, 
-        related_name="zonas", 
-        verbose_name="Municipio Asignado",
-        db_column='MunicipioID'
+        Municipio,
+        on_delete=models.PROTECT,
+        db_column='id_municipio'
     )
-    descripcion = models.TextField(blank=True, null=True, verbose_name="Descripción o Referencia")
-    fecha_registro = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name = "Zona"
-        verbose_name_plural = "Zonas"
-        db_table = 'Zonas'
-        unique_together = ('nombre', 'municipio')
+        db_table = 'Zona'
 
     def __str__(self):
-        return f"{self.nombre} - {self.municipio.nombre}"
+        return self.nombre
 
 
 class Red(models.Model):
-    RedID = models.AutoField(primary_key=True, verbose_name="ID Red")
-    nombre = models.CharField(max_length=100, unique=True, verbose_name="Nombre de la Red")
+    id_red = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
 
     zona = models.ForeignKey(
-        Zona, 
-        on_delete=models.PROTECT, 
-        related_name="redes", 
-        verbose_name="Zona Territorial",
-        db_column='ZonaID'
+        Zona,
+        on_delete=models.PROTECT,
+        db_column='id_zona'
     )
-    capacidad_maxima = models.DecimalField(
-        max_digits=10, 
-        decimal_places=2, 
-        help_text="Capacidad en kW", 
-        verbose_name="Capacidad Máxima"
-    )
-    fecha_registro = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name = "Red Eléctrica"
-        verbose_name_plural = "Redes Eléctricas"
-        db_table = 'Redes'
+        db_table = 'Red'
 
     def __str__(self):
-        return f"{self.nombre} (Zona: {self.zona.nombre})"
+        return self.nombre
 
 class Luminaria(models.Model):
-    codigo_unico = models.CharField(max_length=50, unique=True, verbose_name="Código luminaria")
-    red = models.ForeignKey(Red, on_delete=models.PROTECT, related_name="luminarias", verbose_name="Red Conectada")
-    estado = models.BooleanField(default=True, verbose_name="Estado de Operación")
+    id_luminaria = models.AutoField(primary_key=True)
+    potencia = models.CharField(max_length=255, blank=True, null=True)
+    ubicacion = models.CharField(max_length=255, blank=True, null=True)
+    fecha_instalacion = models.DateField(blank=True, null=True)
+
+    red = models.ForeignKey(
+        Red,
+        on_delete=models.PROTECT,
+        db_column='id_red'
+    )
 
     class Meta:
-        verbose_name = "Luminaria"
-        verbose_name_plural = "Luminarias"
-        db_table='Luminarias'
+        db_table = 'Luminaria'
 
     def __str__(self):
-        return f"Lumi {self.codigo_unico} ({self.red.nombre})"
+        return f"Luminaria {self.id_luminaria}"
 
 
 class RegistroConsumo(models.Model):
-    luminaria = models.ForeignKey(Luminaria, on_delete=models.CASCADE, related_name="consumos", verbose_name="Luminaria")
-    registrado_por = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name="Técnico Responsable")
-    fecha_registro = models.DateTimeField(auto_now_add=True)
+    id_registro = models.AutoField(primary_key=True)
+
+    consumo_kwh = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    costo = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    periodo_inicio = models.DateField()
+    fecha_registro = models.DateField(auto_now_add=True)
+    periodo_fin = models.DateField()
+
+    luminaria = models.ForeignKey(
+        Luminaria,
+        on_delete=models.PROTECT,
+        db_column='id_luminaria'
+    )
+
+    tecnico = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        db_column='id_tecnico'
+    )
 
     class Meta:
-        verbose_name = "registro de Consumo"
-        verbose_name_plural = "Registros de Consumo"
-        db_table='RegistrosConsumo'
-
-    def save(self, *args, **kwargs):
-        tarifa_kwh = 0.15 
-        self.costo_calculado = float(self.consumo_kwh) * tarifa_kwh
-        super().save(*args, **kwargs)
+        db_table = 'RegistroConsumo'
 
     def __str__(self):
-        return f"Consumo es xd"
+        return f"Registro {self.id_registro}"
+    
+
+class Reporte(models.Model):
+    id_reporte = models.AutoField(primary_key=True, verbose_name="ID Reporte")
+    fecha_generacion = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Generación")
+    tipo_nivel = models.CharField(max_length=100, verbose_name="Tipo de Nivel")
+    fecha_inicio = models.DateField(verbose_name="Fecha de Inicio")
+    fecha_fin = models.DateField(verbose_name="Fecha de Fin")
+    total_kwh = models.DecimalField(max_digits=12, decimal_places=2, verbose_name="Total kWh")
+    total_costo = models.DecimalField(max_digits=12, decimal_places=2, verbose_name="Total Costo")
+
+    # Relación con el usuario administrador
+    admin = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name="reportes_generados",
+        verbose_name="Administrador Responsable"
+    )
+
+    class Meta:
+        verbose_name = "Reporte"
+        verbose_name_plural = "Reportes"
+        db_table = 'Reporte'
+
+    def __str__(self):
+        return f"Reporte {self.id_reporte} - {self.fecha_generacion.date()}"
