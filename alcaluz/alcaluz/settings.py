a6cedee300a0588df2dbe9f5ba3897ab407b7e68
CoradoSSelvin <cm23042@ues.edu.sv>
2026-06-16 18:14:58 -0600

    miniajuste en settings.py

--- alcaluz/alcaluz/settings.py
+++ alcaluz/alcaluz/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'alcaluzdb',
         'USER': 'postgres',
-        'PASSWORD': 'mm24067',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '5432',
     }
